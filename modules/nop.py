7fef85f75398b6c22aded9133b6731b1e811a5d6
Ryan Goh <1802980@sit.singaporetech.edu.sg>
2022-03-15 00:10:03 +0800

    Added NOP and method_rename modules.

--- modules/nop.py
+++ modules/nop.py
@@ -1,30 +1,54 @@
 import re
-import random 
+import secrets
+from utils import * 
 
 
+class Nop:
+	def __init__(self, smali_file_list):
+		self.smali_file_list = smali_file_list
+		self.__valid_nop_op_codes_dir = r"C:\Users\tux\Documents\GitHub\ICT-2207-A2\modules\resources\op_codes.txt"
+		self.__op_code_pattern = re.compile(r"\s+(?P<op_code>\S+)")
+
+	def retrieve_valid_nop_op_codes(self):
+		op_code_file = open(self.__valid_nop_op_codes_dir, "r") 
+		valid_op_codes_list = []
+		for line in op_code_file:
+			valid_op_codes_list.append(line.strip("\n"))
+		return valid_op_codes_list
+
+	def secure_random_number(self):
+		secure_rng = secrets.SystemRandom()
+		return secure_rng.randrange(3, 9)
+
+	def insert_nop(self, valid_op_codes_list, smali_file):
+		with inplace_edit_file(smali_file) as (in_file, out_file):
+			for line in in_file:
+				out_file.write(line)
+				match = self.__op_code_pattern.match(line)
+				if match:
+					op_code = match.group("op_code")
+					if op_code in valid_op_codes_list:
+						nop_count = self.secure_random_number()
+						out_file.write("\tnop\n" * nop_count)		
+		in_file.close()
+		out_file.close()
+			
+	def run(self):
+		if self.smali_file_list is None:
+			return False 
+
+		else:
+			# Retrieve valid op codes 
+			valid_op_codes_list = self.retrieve_valid_nop_op_codes()
+
+			# Operate on each smali file 
+			for smali_file in self.smali_file_list:
+				self.insert_nop(valid_op_codes_list, smali_file)
+				
+			return True
 
 
-def get_nop_valid_op_codes():
-	f = open(r"C:\Users\tux\Documents\GitHub\ICT-2207-A2\modules\op_codes.txt", "r") 
-	valid_op_codes = []
-	for line in f:
-		valid_op_codes.append(line.strip("\n"))
-	return valid_op_codes
-
-
-
-f = open(r"C:\Users\tux\Documents\GitHub\ICT-2207-A2\modules\MainActivity.smali", "r")
-f1 = open(r"C:\Users\tux\Documents\GitHub\ICT-2207-A2\modules\uwu.smali", "w")
-op_codes = get_nop_valid_op_codes()
-pattern = re.compile(r"\s+(?P<op_code>\S+)")
-
-
-for line in f:
-	f1.write(line)
-	match = pattern.match(line)
-	if match:
-		op_code = match.group("op_code")
-		if op_code in op_codes:
-			nop_count = random.randint(3, 9)
-			f1.write("\tnop\n" * nop_count)
-
+if __name__ == "__main__":
+	smali_files_list = get_smali_files_list("A2_SAMPLE_ALT")
+	nop = Nop(smali_files_list) 
+	nop.run()
--- modules/utils.py
+++ modules/utils.py
@@ -1,54 +1,86 @@
 import os
+import shutil
 from contextlib import contextmanager
+from pathlib import Path
 
 
 # Adapted from https://www.zopatista.com/python/2013/11/26/inplace-file-rewriting/
 @contextmanager
 def inplace_edit_file(filename):
-    """
-    Allow for a file to be replaced with new content.
-    Yield a tuple of (readable, writable) file objects, where writable replaces
-    readable. If an exception occurs, the old file is restored, removing the
-    written data.
-    """
+	"""
+	Allow for a file to be replaced with new content.
+	Yield a tuple of (readable, writable) file objects, where writable replaces
+	readable. If an exception occurs, the old file is restored, removing the
+	written data.
+	"""
 
-    backup_filename = "{0}{1}{2}".format(filename, os.extsep, "bak")
+	backup_filename = "{0}{1}{2}".format(filename, os.extsep, "bak")
 
-    try:
-        os.unlink(backup_filename)
-    except OSError:
-        pass
-    os.rename(filename, backup_filename)
+	try:
+		os.unlink(backup_filename)
+	except OSError:
+		pass
+	os.rename(filename, backup_filename)
 
-    readable = open(backup_filename, "r", encoding="utf-8")
-    try:
-        perm = os.fstat(readable.fileno()).st_mode
-    except OSError:
-        writable = open(filename, "w", encoding="utf-8", newline="")
-    else:
-        os_mode = os.O_CREAT | os.O_WRONLY | os.O_TRUNC
-        if hasattr(os, "O_BINARY"):
-            os_mode |= os.O_BINARY
-        fd = os.open(filename, os_mode, perm)
-        writable = open(fd, "w", encoding="utf-8", newline="")
-        try:
-            if hasattr(os, "chmod"):
-                os.chmod(filename, perm)
-        except OSError:
-            pass
-    try:
-        yield readable, writable
-    except Exception as e:
-        try:
-            os.unlink(filename)
-        except OSError:
-            pass
-        os.rename(backup_filename, filename)
-        raise
-    finally:
-        readable.close()
-        writable.close()
-        try:
-            os.unlink(backup_filename)
-        except OSError:
-            pass
+	readable = open(backup_filename, "r", encoding="utf-8")
+	try:
+		perm = os.fstat(readable.fileno()).st_mode
+	except OSError:
+		writable = open(filename, "w", encoding="utf-8", newline="")
+	else:
+		os_mode = os.O_CREAT | os.O_WRONLY | os.O_TRUNC
+		if hasattr(os, "O_BINARY"):
+			os_mode |= os.O_BINARY
+		fd = os.open(filename, os_mode, perm)
+		writable = open(fd, "w", encoding="utf-8", newline="")
+		try:
+			if hasattr(os, "chmod"):
+				os.chmod(filename, perm)
+		except OSError:
+			pass
+	try:
+		yield readable, writable
+	except Exception as e:
+		try:
+			os.unlink(filename)
+		except OSError:
+			pass
+		os.rename(backup_filename, filename)
+		raise
+	finally:
+		readable.close()
+		writable.close()
+		try:
+			os.unlink(backup_filename)
+		except OSError:
+			pass
+
+
+def get_smali_files_list(apk_name):
+	path = Path(os.getcwd())
+	parent_dir = path.parent.absolute()
+	smali_dir_path = os.path.join(parent_dir, "dumpster", apk_name, 'smali')
+	list_of_smali_files = []
+
+	for root, d_names, f_names in os.walk(smali_dir_path):
+		if f_names != []:
+			for item in f_names:
+				smali_file_path = os.path.join(root, item)
+				list_of_smali_files.append(smali_file_path)
+	return list_of_smali_files 
+
+
+# def create_backup_smali_files(smali_file_list):
+# 	for smali_file in smali_file_list:
+
+
+# 		bak_file_name = "%s.bak" % smali_file
+# 		dst_path = os.path.join(smali_file_path, bak_file_name)
+# 		shutil.copy(src_path, dst_path)		
+
+
+# def remove_backup_smali_files(smali_file_list):
+# 	for smali_file in smali_file_list:
+# 		bak_file_name = "%s.bak" % smali_file 
+# 		dst_path = os.path.join(bak_file_name)
+# 		os.remove(dst_path)
