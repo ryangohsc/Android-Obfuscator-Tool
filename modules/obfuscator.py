c7a74af14206a306d0b66c5570888fa9d22ad3d5
Ryan Goh <1802980@sit.singaporetech.edu.sg>
2022-03-19 22:35:41 +0800

    Added fix to nop

--- modules/obfuscator.py
+++ modules/obfuscator.py
@@ -17,26 +17,23 @@ def run(TMP_ASSET_FOLDER, WORKING_FOLDER, APK_NAME, OBFUSCATION_METHODS):
     WORKING_SMALI_LOC_FILE = "smali.txt"
     WORKING_COPY_DIR = os.path.join(WORKING_FOLDER, APK_NAME.replace('.apk', ''))
 
-    #
     # Does not require smali file, but should run first so other modules can obfuscate it
     # Function will always run regardless of user choice
     dci = DefunctClassInsertion()
     dci.run(WORKING_COPY_DIR)
     OBFUSCATION_METHODS.pop("dci")
 
-    #
     # Load locations of all smali files
     p = os.path.join(TMP_ASSET_FOLDER, WORKING_SMALI_LOC_FILE)
     f = open(p, "r")
     smali_locations = f.readlines()
     f.close()
 
-    #
     # Do something here for the different modules
     dmi = DefunctMethodInsertion()
     uj = UnconditionalJump()
     ab = ArithmeticBranching()
-    # nop = Nop()
+    nop = Nop()
     method_rename = MethodRename()
     variable_rename = VariableRename()
     ro = Reorder()
@@ -46,7 +43,7 @@ def run(TMP_ASSET_FOLDER, WORKING_FOLDER, APK_NAME, OBFUSCATION_METHODS):
         if OBFUSCATION_METHODS["dmi"]: dmi.run(file)  # should run second so other modules can obfuscate it
         if OBFUSCATION_METHODS["uj"]: uj.run(file)
         if OBFUSCATION_METHODS["ab"]: ab.run(file)
-        # if OBFUSCATION_METHODS["nop"]: nop.run(file)
+        if OBFUSCATION_METHODS["nop"]: nop.run(file)
         if OBFUSCATION_METHODS["method_rename"]: method_rename.run(file)
         if OBFUSCATION_METHODS["variable_rename"]: variable_rename.run(file)
         if OBFUSCATION_METHODS["ro"]: ro.run(file)  # should run last
