e10c607eade3d1cd8eaf3358fda6ca28a1a4e654
takaramirin <2000819@sit.singaporetech.edu.sg>
2022-03-22 01:57:49 +0800

    refactoring

--- modules/arithmetic_branching.py
+++ modules/arithmetic_branching.py
@@ -24,6 +24,17 @@ class ArithmeticBranching:
         """
         return ''.join(random.SystemRandom().choice(string.ascii_letters) for _ in range(16))
 
+    def check_non_abstract(self, arg_line):
+        """
+        Checks for non-abstract methods
+        :param arg_line:
+        :return True:
+        :return False:
+        """
+        if arg_line.startswith(".method ") and " abstract " not in arg_line and " native " not in arg_line:
+            return True
+        return False
+
     def run(self, arg_filename):
         """
         Run the arithmetic branching module
@@ -33,43 +44,39 @@ class ArithmeticBranching:
         edit_method = False
         start_label = None
         end_label = None
-        try:
-            with inplace_edit_file(arg_filename) as (input_file, output_file):
-                for line in input_file:
-                    # check for non-abstract or native methods
-                    if line.startswith(
-                            ".method ") and " abstract " not in line and " native " not in line and not edit_method:
-                        # in a method, can edit
-                        output_file.write(line)
-                        edit_method = True
-                    elif line.startswith(".end method") and edit_method:
-                        # end of method and disable edit
-                        if start_label and end_label:
-                            output_file.write("\t:%s\n" % end_label)
-                            output_file.write("\tgoto/32 :%s\n" % start_label)
-                            start_label = None
-                            end_label = None
-                        output_file.write(line)
-                        edit_method = False
-                    elif edit_method:
-                        output_file.write(line)
-                        # match for locals line
-                        match = self.locals_pattern.match(line)
-                        # check if local count is 2, means 2 registers is available, then can do arithmetic branching
-                        if match and int(match.group("local_count")) >= 2:
-                            # generate random number and strings
-                            start_label = self.random_string()
-                            end_label = self.random_string()
-                            tmp_label = self.random_string()
-                            output_file.write("\n\tconst v0, %s\n" % self.random_number())
-                            output_file.write("\tconst v1, %s\n" % self.random_number())
-                            output_file.write("\tadd-int v0, v0, v1\n")
-                            output_file.write("\trem-int v0, v0, v1\n")
-                            output_file.write("\tif-gtz v0, :%s\n" % tmp_label)
-                            output_file.write("\tgoto/32 :%s\n" % end_label)
-                            output_file.write("\t:%s\n" % tmp_label)
-                            output_file.write("\t:%s\n" % start_label)
-                    else:
-                        output_file.write(line)
-        except Exception as e:
-            print(e)
+        with inplace_edit_file(arg_filename) as (input_file, output_file):
+            for line in input_file:
+                # check for non-abstract or native methods
+                if self.check_non_abstract(line) and not edit_method:
+                    # in a method, can edit
+                    output_file.write(line)
+                    edit_method = True
+                elif line.startswith(".end method") and edit_method:
+                    # end of method and disable edit
+                    if start_label and end_label:
+                        output_file.write("\t:%s\n" % end_label)
+                        output_file.write("\tgoto/32 :%s\n" % start_label)
+                        start_label = None
+                        end_label = None
+                    output_file.write(line)
+                    edit_method = False
+                elif edit_method:
+                    output_file.write(line)
+                    # match for locals line
+                    match = self.locals_pattern.match(line)
+                    # check if local count is 2, means 2 registers is available, then can do arithmetic branching
+                    if match and int(match.group("local_count")) >= 2:
+                        # generate random number and strings
+                        start_label = self.random_string()
+                        end_label = self.random_string()
+                        tmp_label = self.random_string()
+                        output_file.write("\n\tconst v0, %s\n" % self.random_number())
+                        output_file.write("\tconst v1, %s\n" % self.random_number())
+                        output_file.write("\tadd-int v0, v0, v1\n")
+                        output_file.write("\trem-int v0, v0, v1\n")
+                        output_file.write("\tif-gtz v0, :%s\n" % tmp_label)
+                        output_file.write("\tgoto/32 :%s\n" % end_label)
+                        output_file.write("\t:%s\n" % tmp_label)
+                        output_file.write("\t:%s\n" % start_label)
+                else:
+                    output_file.write(line)
